69e0982dae944e7a53ea9f92cdf903da1a66b361
mkuhn <michel.kuhn@adition.com>
2019-05-28 10:39:32 +0200

    youtube links

--- content.js
+++ content.js
@@ -15,19 +15,25 @@ if (window === top) {
 trigger_key = 71; // g key
 function doKeyPress(e){
     if (e.shiftKey && e.keyCode === trigger_key){
+        let pageHtml = document.documentElement.outerHTML;
+
+        let youtubeRegex = new RegExp("(https?:\\/\\/)?(www\\.youtube\\.com|youtu\\.?be)\\/watch\\?v=[a-z0-9]+", "i");
+
         let text = getSelectionText();
 
-        let songName = document.getElementsByClassName("header_with_cover_art-primary_info-title")[0].innerHTML;
-        let artist = document.getElementsByClassName("song_album-info-artist")[0].innerHTML;
-        let album = document.getElementsByClassName("song_album-info-title")[0].innerHTML;
+        let songName = document.getElementsByClassName("header_with_cover_art-primary_info-title")[0].innerHTML.trim();
+        let artist = document.getElementsByClassName("song_album-info-artist")[0].innerHTML.trim();
+        let album = document.getElementsByClassName("song_album-info-title")[0].innerHTML.split(/<(.+)/)[0].trim();
         let geniusLink = window.location.href;
+        let youtubeLink = pageHtml.match(youtubeRegex)[0];
 
         let returnObject = {
             "line": text,
             "song": songName,
             "artist": artist,
             "album": album,
-            "genius": geniusLink
+            "genius": geniusLink,
+            "youtube": youtubeLink
         };
 
         console.log(returnObject);
