4fc39da7d34e832860f9f6539dacc88caeefd98f
mkuhn <michel.kuhn@adition.com>
2019-05-28 13:24:06 +0200

    spotify

--- content.js
+++ content.js
@@ -12,7 +12,8 @@ function getSelectionText() {
 if (window === top) {
     window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
 }
-trigger_key = 71; // g key
+
+let trigger_key = 71; // g key
 function doKeyPress(e){
     if (e.shiftKey && e.keyCode === trigger_key){
         let pageHtml = document.documentElement.outerHTML;
@@ -28,16 +29,49 @@ function doKeyPress(e){
         let geniusLink = window.location.href;
         let youtubeLink = pageHtml.match(youtubeRegex)[0];
 
-        let returnObject = {
-            "line": text,
-            "song": songName,
-            "artist": artist,
-            "album": album,
-            "year": year,
-            "genius": geniusLink,
-            "youtube": youtubeLink
-        };
+        $.ajax({
+            type: "GET",
+            url: "https://api.spotify.com/v1/search?q=" + encodeURIComponent(album) + "&type=album&market=DE&limit=1",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Authorization: Bearer BQD3JedFfX_HUULhfcW7ru0jZ1QWc6qIFoE6-wc320wX3gNuLNXP7B-fFkMtWZakr7tbK8uy1a_wvPJOwE-rL2O1cKMrLneX21m5KSKciSbeNLxdeHGRH9N3zq9F-j36wUDJ8j4U8TYUu7vILBE"
+            },
+            success: function(data) {
+                let albumId = data.albums.items[0].id;
+                $.ajax({
+                    type: "GET",
+                    url: "https://api.spotify.com/v1/albums/" + albumId + "/tracks",
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                        "Authorization": "token"
+                    },
+                    success: function(albumTrackData) {
+                        let tracks = albumTrackData.items;
+                        for(let i=0; i < tracks.length; i++) {
+                            if (tracks[i].name === songName) {
+                                spotifyLink = tracks[i].external_urls.spotify;
 
-        console.log(returnObject);
+                                let returnObject = {
+                                    "line": text,
+                                    "song": songName,
+                                    "artist": artist,
+                                    "album": album,
+                                    "year": year,
+                                    "genius": geniusLink,
+                                    "youtube": youtubeLink,
+                                    "spotify": getSpotifyLink(album, songName)
+                                };
+
+                                console.log(returnObject);
+                            }
+                        }
+                    },
+                    dataType: "json"
+                });
+            },
+            dataType: "json"
+        });
     }
 }
