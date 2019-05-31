a70b3c065b58990a68609e16bfbc2387d292a3f7
mkuhn <michel.kuhn@adition.com>
2019-05-31 10:14:50 +0200

    Läuft

--- content.js
+++ content.js
@@ -14,64 +14,82 @@ if (window === top) {
 }
 
 let trigger_key = 71; // g key
-function doKeyPress(e){
-    if (e.shiftKey && e.keyCode === trigger_key){
+function doKeyPress(e) {
+    if (e.shiftKey && e.keyCode === trigger_key) {
         let pageHtml = document.documentElement.outerHTML;
 
         let youtubeRegex = new RegExp("(https?:\\/\\/)?(www\\.youtube\\.com|youtu\\.?be)\\/watch\\?v=[a-z0-9]+", "i");
 
-        let text = getSelectionText();
+        let text = '"' + getSelectionText() + '"'; //"" for saving the newlines in excel
 
         let songName = document.getElementsByClassName("header_with_cover_art-primary_info-title")[0].innerHTML.trim();
         let artist = document.getElementsByClassName("song_album-info-artist")[0].innerHTML.trim();
         let album = document.getElementsByClassName("song_album-info-title")[0].innerHTML.split(/<(.+)/)[0].trim();
         let year = document.getElementsByClassName("metadata_unit-info metadata_unit-info--text_only")[0].innerHTML.split(/,(.+)/)[1].trim();
         let geniusLink = window.location.href;
-        let youtubeLink = pageHtml.match(youtubeRegex)[0];
+        let youtubeLink = "todo";
+        if(pageHtml.match(youtubeRegex) !== null) {
+            youtubeLink = pageHtml.match(youtubeRegex)[0];
+        }
 
-        $.ajax({
-            type: "GET",
-            url: "https://api.spotify.com/v1/search?q=" + encodeURIComponent(album) + "&type=album&market=DE&limit=1",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": "Authorization: Bearer BQD3JedFfX_HUULhfcW7ru0jZ1QWc6qIFoE6-wc320wX3gNuLNXP7B-fFkMtWZakr7tbK8uy1a_wvPJOwE-rL2O1cKMrLneX21m5KSKciSbeNLxdeHGRH9N3zq9F-j36wUDJ8j4U8TYUu7vILBE"
-            },
-            success: function(data) {
-                let albumId = data.albums.items[0].id;
-                $.ajax({
-                    type: "GET",
-                    url: "https://api.spotify.com/v1/albums/" + albumId + "/tracks",
-                    headers: {
-                        "Accept": "application/json",
-                        "Content-Type": "application/json",
-                        "Authorization": "token"
-                    },
-                    success: function(albumTrackData) {
-                        let tracks = albumTrackData.items;
-                        for(let i=0; i < tracks.length; i++) {
-                            if (tracks[i].name === songName) {
-                                spotifyLink = tracks[i].external_urls.spotify;
+        chrome.storage.sync.get(['token'], function(result) {
+            $.ajax({
+                type: "GET",
+                url: "https://api.spotify.com/v1/search?q=" + encodeURIComponent(album) + "&type=album&market=DE&limit=1",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": "Authorization: Bearer " + result.token
+                },
+                success: function (data) {
+                    let albumId = data.albums.items[0].id;
+                    $.ajax({
+                        type: "GET",
+                        url: "https://api.spotify.com/v1/albums/" + albumId + "/tracks",
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json",
+                            "Authorization": "Authorization: Bearer " + result.token
+                        },
+                        success: function (albumTrackData) {
+                            let tracks = albumTrackData.items;
+                            for (let i = 0; i < tracks.length; i++) {
+                                if (tracks[i].name === songName) {
+                                    spotifyLink = tracks[i].external_urls.spotify;
 
-                                let returnObject = {
-                                    "line": text,
-                                    "song": songName,
-                                    "artist": artist,
-                                    "album": album,
-                                    "year": year,
-                                    "genius": geniusLink,
-                                    "youtube": youtubeLink,
-                                    "spotify": getSpotifyLink(album, songName)
-                                };
+                                    let csv = [text, artist, songName, youtubeLink, spotifyLink, geniusLink, album, year];
+                                    chrome.storage.sync.get(["csvRows"], function(result) {
+                                        let rowArray = result.csvRows;
+                                        rowArray.push(csv);
 
-                                console.log(returnObject);
+                                        chrome.storage.sync.set({csvRows: rowArray}, function() {
+                                            console.log("Saved a new array item");
+                                            alert(text);
+                                        });
+                                    });
+                                }
                             }
-                        }
-                    },
-                    dataType: "json"
-                });
-            },
-            dataType: "json"
+                        },
+                        dataType: "json"
+                    });
+                },
+                error: function () {
+                    alert("Spotify macht Heckmeck, speichere ohne Link. Vielleicht Token erneuern.");
+                    spotifyLink = "";
+
+                    let csv = [text, artist, songName, youtubeLink, spotifyLink, geniusLink, album, year];
+                    chrome.storage.sync.get(["csvRows"], function (result) {
+                        let rowArray = result.csvRows;
+                        rowArray.push(csv);
+
+                        chrome.storage.sync.set({csvRows: rowArray}, function () {
+                            console.log("Saved a new array item");
+                            alert(text);
+                        });
+                    });
+                },
+                dataType: "json"
+            });
         });
     }
 }
---
+++ popup.js
@@ -0,0 +1,36 @@
+let tokenInput = document.getElementById("token");
+let sendButton = document.getElementById("sendToken");
+let csvButton = document.getElementById("downloadCsv");
+
+sendButton.addEventListener('click', function () {
+    let spotifyToken = tokenInput.value;
+    alert("lol");
+    chrome.storage.sync.set({token: spotifyToken}, function() {
+        console.log("Token: " + spotifyToken)
+    });
+    chrome.storage.sync.set({csvRows: []}, function () {
+        console.log("Reset rows");
+    });
+});
+
+csvButton.addEventListener('click', function () {
+    chrome.storage.sync.get(['csvRows'], function (result) {
+        let lineArray = [];
+        result.csvRows.forEach(function (infoArray, index) {
+            let line = infoArray.join(";");
+            lineArray.push(index === 0 ? "data:text/csv;charset=utf-8," + line : line);
+        });
+        let csvContent = lineArray.join("\n");
+
+        let encodedUri = encodeURI(csvContent);
+        let link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "my_data.csv");
+        document.body.appendChild(link); // Required for FF
+
+        link.click()
+    });
+    chrome.storage.sync.set({csvRows: []}, function () {
+        console.log("Reset rows");
+    })
+});
